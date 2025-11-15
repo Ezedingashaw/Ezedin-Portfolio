fe210b5a909ec1fab2afd4606dc8917c0b8f2d7d
Ezedin G <ezedingashaw@gmail.com>
2025-11-15 02:19:46 -0800

    add tailwind style setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: "hsl(var(--card))",
+        "card-foreground": "hsl(var(--card-foreground))",
+        ring: "hsl(var(--ring))",
+      },
+    },
+  },
+  plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig({
+  plugins: [
+    react(),
+  tailwindcss(),
+],
+  resolve: {
+    alias: {
+  "@": path.resolve(__dirname, "./src"),
+},
 
-  return {
-    plugins: [react()],
-    // base: env.VITE_BASE_PATH || "/Ezedin-Portfolio",
-  };
-});
+  },
+})
